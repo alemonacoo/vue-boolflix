bfbb52a16b82d53fd2e729821b671176cb7d54b1
alemonacoo <ale.monaco@outlook.com>
2022-10-03 11:54:47 +0200

    Adding font awesome

--- src/components/voteComponent.vue
+++ src/components/voteComponent.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <FontAwesomeIcon icon="fa-solid fa-star" />
+    <font-awesome-icon icon="fa-regular fa-star" />
+    <font-awesome-icon icon="fa-solid fa-star" />
+
     {{getOneToFive(vote)}}
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar as faStarSolid } from '@fortawesome/free-regular-svg-icons'
+import { faStar as faStarRegular} from '@fortawesome/free-solid-svg-icons'
+library.add(faStarSolid, faStarRegular);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
+
+
 export default {
     name: 'VoteComponent',
     props:{
