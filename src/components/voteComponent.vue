32d69203a377541541cdb81598f47229d677be7a
alemonacoo <ale.monaco@outlook.com>
2022-10-03 12:30:00 +0200

    Milestone 3 Complete!

--- src/components/voteComponent.vue
+++ src/components/voteComponent.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <font-awesome-icon icon="fa-regular fa-star" />
-    <font-awesome-icon icon="fa-solid fa-star" />
-
-    {{getOneToFive(vote)}}
+  <div class="StarsContainer">
+    <div>
+    <font-awesome-icon v-for="fullStar in getStars(vote)" 
+    :key="fullStar" icon="fa-solid fa-star" />
+    </div>
+    <div>  
+      <font-awesome-icon v-for="emptyStar in (MAX_STARS - getStars(vote))" 
+      :key="emptyStar" icon="fa-regular fa-star" />
+    </div>
   </div>
 </template>
 
 <script>
+//Importing fontawesome star icons
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -17,21 +22,35 @@ library.add(faStarSolid, faStarRegular);
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
+//Numer of Stars
+const MAX_STARS = 5;
+
 
 export default {
     name: 'VoteComponent',
     props:{
         vote: Number,
     }, 
+    data(){
+      return{
+        MAX_STARS,
+      }
+    },
     methods:{
-        getOneToFive(n){
-            return Math.ceil(n/2);
+        getStars(n){
+          if(n === 0){
+            return 1;
+          }
+            return Math.ceil(n / (10 / MAX_STARS));
         }
     }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoper>
+.StarsContainer{
+  display: flex;
+}
 
 </style>
